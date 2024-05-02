9982327cdf94a206f348874f3fcaee1f81c6dec2
MatheusCFBT <matheuscaldanaf@gmail.com>
2024-05-02 14:05:31 -0300

    Add project files.

---
+++ RegistrationSystem/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace RegistrationSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTelefone = new System.Windows.Forms.MaskedTextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEC = new System.Windows.Forms.ComboBox();
+            this.txtNasc = new System.Windows.Forms.DateTimePicker();
+            this.checkCasa = new System.Windows.Forms.CheckBox();
+            this.checkVeiculo = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioO = new System.Windows.Forms.RadioButton();
+            this.radioF = new System.Windows.Forms.RadioButton();
+            this.radioM = new System.Windows.Forms.RadioButton();
+            this.btnCad = new System.Windows.Forms.Button();
+            this.btnEx = new System.Windows.Forms.Button();
+            this.btnLim = new System.Windows.Forms.Button();
+            this.lista = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Data de nascimento:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Estado civil:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(8, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Telefone:";
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTelefone.Location = new System.Drawing.Point(12, 228);
+            this.txtTelefone.Mask = "(99) 00000-0000";
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(117, 26);
+            this.txtTelefone.TabIndex = 4;
+            this.txtTelefone.TextMaskFormat = System.Windows.Forms.MaskFormat.IncludePromptAndLiterals;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(12, 46);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(502, 26);
+            this.txtNome.TabIndex = 5;
+            // 
+            // txtEC
+            // 
+            this.txtEC.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEC.FormattingEnabled = true;
+            this.txtEC.Items.AddRange(new object[] {
+            "Solteiro(a)",
+            "Casado(a)",
+            "Divorciado(a)",
+            "Viúvo(a)"});
+            this.txtEC.Location = new System.Drawing.Point(12, 166);
+            this.txtEC.Name = "txtEC";
+            this.txtEC.Size = new System.Drawing.Size(318, 26);
+            this.txtEC.TabIndex = 6;
+            // 
+            // txtNasc
+            // 
+            this.txtNasc.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNasc.Location = new System.Drawing.Point(12, 106);
+            this.txtNasc.Name = "txtNasc";
+            this.txtNasc.Size = new System.Drawing.Size(318, 26);
+            this.txtNasc.TabIndex = 7;
+            // 
+            // checkCasa
+            // 
+            this.checkCasa.AutoSize = true;
+            this.checkCasa.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkCasa.Location = new System.Drawing.Point(12, 269);
+            this.checkCasa.Name = "checkCasa";
+            this.checkCasa.Size = new System.Drawing.Size(147, 22);
+            this.checkCasa.TabIndex = 8;
+            this.checkCasa.Text = "Possui casa própria";
+            this.checkCasa.UseVisualStyleBackColor = true;
+            // 
+            // checkVeiculo
+            // 
+            this.checkVeiculo.AutoSize = true;
+            this.checkVeiculo.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkVeiculo.Location = new System.Drawing.Point(12, 296);
+            this.checkVeiculo.Name = "checkVeiculo";
+            this.checkVeiculo.Size = new System.Drawing.Size(117, 22);
+            this.checkVeiculo.TabIndex = 9;
+            this.checkVeiculo.Text = "Possui veículo";
+            this.checkVeiculo.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioO);
+            this.groupBox1.Controls.Add(this.radioF);
+            this.groupBox1.Controls.Add(this.radioM);
+            this.groupBox1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 323);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(263, 126);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sexo:";
+            // 
+            // radioO
+            // 
+            this.radioO.AutoSize = true;
+            this.radioO.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioO.Location = new System.Drawing.Point(6, 87);
+            this.radioO.Name = "radioO";
+            this.radioO.Size = new System.Drawing.Size(65, 22);
+            this.radioO.TabIndex = 2;
+            this.radioO.TabStop = true;
+            this.radioO.Text = "Outro";
+            this.radioO.UseVisualStyleBackColor = true;
+            // 
+            // radioF
+            // 
+            this.radioF.AutoSize = true;
+            this.radioF.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioF.Location = new System.Drawing.Point(6, 59);
+            this.radioF.Name = "radioF";
+            this.radioF.Size = new System.Drawing.Size(88, 22);
+            this.radioF.TabIndex = 1;
+            this.radioF.TabStop = true;
+            this.radioF.Text = "Feminino";
+            this.radioF.UseVisualStyleBackColor = true;
+            // 
+            // radioM
+            // 
+            this.radioM.AutoSize = true;
+            this.radioM.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioM.Location = new System.Drawing.Point(6, 31);
+            this.radioM.Name = "radioM";
+            this.radioM.Size = new System.Drawing.Size(92, 22);
+            this.radioM.TabIndex = 0;
+            this.radioM.TabStop = true;
+            this.radioM.Text = "Masculino";
+            this.radioM.UseVisualStyleBackColor = true;
+            // 
+            // btnCad
+            // 
+            this.btnCad.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCad.Location = new System.Drawing.Point(8, 468);
+            this.btnCad.Name = "btnCad";
+            this.btnCad.Size = new System.Drawing.Size(182, 38);
+            this.btnCad.TabIndex = 11;
+            this.btnCad.Text = "Cadastrar / Alterar";
+            this.btnCad.UseVisualStyleBackColor = true;
+            this.btnCad.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnEx
+            // 
+            this.btnEx.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEx.Location = new System.Drawing.Point(222, 468);
+            this.btnEx.Name = "btnEx";
+            this.btnEx.Size = new System.Drawing.Size(149, 38);
+            this.btnEx.TabIndex = 12;
+            this.btnEx.Text = "Excluir";
+            this.btnEx.UseVisualStyleBackColor = true;
+            this.btnEx.Click += new System.EventHandler(this.btnEx_Click);
+            // 
+            // btnLim
+            // 
+            this.btnLim.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLim.Location = new System.Drawing.Point(403, 468);
+            this.btnLim.Name = "btnLim";
+            this.btnLim.Size = new System.Drawing.Size(149, 38);
+            this.btnLim.TabIndex = 13;
+            this.btnLim.Text = "Limpar";
+            this.btnLim.UseVisualStyleBackColor = true;
+            this.btnLim.Click += new System.EventHandler(this.btnLim_Click);
+            // 
+            // lista
+            // 
+            this.lista.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lista.FormattingEnabled = true;
+            this.lista.ItemHeight = 21;
+            this.lista.Location = new System.Drawing.Point(681, 46);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(263, 403);
+            this.lista.TabIndex = 14;
+            this.lista.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lista_MouseDoubleClick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(677, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 24);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Cadastros:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(956, 528);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lista);
+            this.Controls.Add(this.btnLim);
+            this.Controls.Add(this.btnEx);
+            this.Controls.Add(this.btnCad);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.checkVeiculo);
+            this.Controls.Add(this.checkCasa);
+            this.Controls.Add(this.txtNasc);
+            this.Controls.Add(this.txtEC);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox txtTelefone;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.ComboBox txtEC;
+        private System.Windows.Forms.DateTimePicker txtNasc;
+        private System.Windows.Forms.CheckBox checkCasa;
+        private System.Windows.Forms.CheckBox checkVeiculo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioF;
+        private System.Windows.Forms.RadioButton radioM;
+        private System.Windows.Forms.RadioButton radioO;
+        private System.Windows.Forms.Button btnCad;
+        private System.Windows.Forms.Button btnEx;
+        private System.Windows.Forms.Button btnLim;
+        private System.Windows.Forms.ListBox lista;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ RegistrationSystem/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegistrationSystem
+{
+    public partial class Form1 : Form
+    {
+        List<Pessoa> pessoas; // Representa uma lista fortemente tipada de objetos que podem ser acessados por índice. 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int index = -1; // Está -1 pois o indice começa com 0
+
+            foreach (Pessoa pessoa in pessoas)
+            {
+                if (pessoa.Nome == txtNome.Text)
+                {
+                    index = pessoas.IndexOf(pessoa);
+                }
+            }
+
+            if (txtNome.Text == "")
+            {
+                MessageBox.Show("Nome inválido. Preencha o campo corretamente.");
+                txtNome.Focus();
+                return;
+            }
+
+            if (!txtTelefone.MaskCompleted)
+            {
+                MessageBox.Show("Telefone inválido. Preencha o campo corretamente");
+                txtTelefone.Focus();
+                return;
+            }
+
+            char sexo;
+
+            if (radioM.Checked)
+            {
+                sexo = 'M';
+            }
+            else if (radioF.Checked)
+            {
+                sexo = 'F';
+            }
+            else
+            {
+                sexo = 'O';
+            }
+
+            Pessoa p = new Pessoa();
+            p.Nome = txtNome.Text;
+            p.DataNascimento = txtNasc.Text;
+            p.EstadoCivil = txtEC.Text;
+            p.Telefone = txtTelefone.Text;
+            p.CasaPropria = checkCasa.Checked;
+            p.Veiculo = checkVeiculo.Checked;
+            p.Sexo = sexo;
+
+            if (index < 0)
+            {
+                pessoas.Add(p);
+            }
+            else
+            {
+                pessoas[index] = p;
+            }
+
+            btnLim_Click(btnLim, EventArgs.Empty);
+
+            Listar();
+        }
+
+        private void btnEx_Click(object sender, EventArgs e)
+        {
+            int indice = lista.SelectedIndex;
+            pessoas.RemoveAt(indice);
+            Listar();
+        }
+
+        private void btnLim_Click(object sender, EventArgs e)
+        {
+            txtNome.Text = "";
+            txtNasc.Text = "";
+            txtEC.SelectedIndex = 0;
+            txtTelefone.Text = "";
+            checkCasa.Checked = false;
+            checkVeiculo.Checked = false;
+            radioM.Checked = true;
+            radioF.Checked = false;
+            radioO.Checked = false;
+            txtNome.Focus();
+        }
+
+        private void Listar()
+        {
+            lista.Items.Clear();
+
+            foreach (Pessoa p in pessoas)
+            {
+                lista.Items.Add(p.Nome);
+            }
+        }
+
+        private void lista_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int indice = lista.SelectedIndex;
+            Pessoa p = pessoas[indice];
+
+            txtNome.Text = p.Nome;
+            txtNasc.Text = p.DataNascimento;
+            txtEC.SelectedItem = p.EstadoCivil;
+            txtTelefone.Text = p.Telefone;
+            checkCasa.Checked = p.CasaPropria;
+            checkVeiculo.Checked = p.Veiculo;
+
+            switch (p.Sexo)
+            {
+                case 'M':
+                    radioM.Checked = true;
+                    break;
+                case 'F':
+                    radioF.Checked = true;
+                    break;
+                default:
+                    radioO.Checked = true;
+                    break;
+            }
+        }
+    }
+}
